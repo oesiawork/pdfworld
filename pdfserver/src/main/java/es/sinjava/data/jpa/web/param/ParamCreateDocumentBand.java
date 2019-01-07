82224855985fefc0596f9a52c401d82ad4570dc9
andre_k <andre_k@DESKTOP10.home>
2019-01-07 15:56:37 +0100

    Añadiendo dependencias

---
+++ pdfserver/src/main/java/es/sinjava/data/jpa/web/param/ParamCreateDocumentBand.java
@@ -0,0 +1,10 @@
+package es.sinjava.data.jpa.web.param;
+
+public class ParamCreateDocumentBand {
+	
+	private BandTemplate bandTemplate;
+	private PdfTemplate pdfTemplate;
+	private FielContainer bandTempletaData;
+	private FielContainer pdfTemplateData;
+
+}
